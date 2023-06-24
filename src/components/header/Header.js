2a171a31c5e37610d2c4bbbe2669468f3ea1f79b
DevAkky <114848776+devakashpandey@users.noreply.github.com>
2023-06-24 16:39:26 +0530

    adding avatar pic

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,6 +5,7 @@ import SearchIcon from "@mui/icons-material/Search";
 import { BsMessenger } from "react-icons/bs";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import { Avatar, IconButton, Tooltip } from "@mui/material";
+import pic from "../../assets/pic.png";
 
 const Header = () => {
   return (
@@ -27,21 +28,21 @@ const Header = () => {
                   style={{
                     color: "black",
                   }}
-                  size={15}
+                  size={14}
                 />
               </IconButton>
             </Tooltip>
             <Tooltip title="Notifications">
               <IconButton>
                 <NotificationsIcon
-                  style={{ color: "black", fontSize: "20px" }}
+                  style={{ color: "black", fontSize: "18px" }}
                 />
               </IconButton>
             </Tooltip>
           </div>
           <div className="avatar">
             <Tooltip title="Account">
-              <Avatar sx={{ width: 35, height: 35 }} />
+              <Avatar alt="Profile" src={pic} sx={{ width: 37, height: 37 }} />
             </Tooltip>
           </div>
         </div>
