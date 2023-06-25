c68928d5e98a1a949455d8c9ed1cbf7fb7064db9
DevAkky <114848776+devakashpandey@users.noreply.github.com>
2023-06-25 10:58:24 +0530

    left Sidebar start

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import "./App.css";
 import Header from "./components/header/Header";
+import LeftSidebar from "./leftSidebar/LeftSidebar";
 
 function App() {
   return (
     <>
       {/*----- HEADER -----*/}
       <Header />
-      {/*------ BODY ------*/}
-      {/* SIDEBAR */}
+
+      {/*---- LEFT SIDEBAR  ----*/}
+      <div className="app-body">
+        <LeftSidebar />
+      </div>
+
       {/* POST */}
       {/* RIGHT SIDEBAR */}
     </>
---
+++ src/leftSidebar/LeftSidebar.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./LeftSidebar.css";
+import SidebarOptions from "./sidebarOptions/SidebarOptions";
+
+const LeftSidebar = () => {
+  return (
+    <>
+      <div>Left SIDEBAR</div>
+      <div>
+        <SidebarOptions />
+      </div>
+    </>
+  );
+};
+
+export default LeftSidebar;
