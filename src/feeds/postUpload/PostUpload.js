296f1e9c7a63543c09a1778ea9d9e2f08bae8090
DevAkky <114848776+devakashpandey@users.noreply.github.com>
2023-06-27 17:01:49 +0530

    upload section done

--- src/feeds/postUpload/PostUpload.js
+++ src/feeds/postUpload/PostUpload.js
@@ -1,19 +1,34 @@
 import { Avatar } from "@mui/material";
 import React from "react";
+import "./PostUpload.css";
+import { RiLiveFill } from "react-icons/ri";
+import { MdPhotoLibrary } from "react-icons/md";
+import { FaRegLaugh } from "react-icons/fa";
+import myPic from "../../assets/pic.png";
 
 const PostUpload = () => {
   return (
     <>
       <div className="post-upload">
-        POST UPLOAD
         <div className="top-section">
-          <Avatar />
+          <Avatar src={myPic} sx={{ height: "45px", width: "45px" }} />
           <form>
             <input placeholder="What's on your mind, Akash?" />
           </form>
         </div>
         <div className="bottom-section">
-          <div className="upload-option"></div>
+          <div className="upload-option">
+            <RiLiveFill style={{ color: "red" }} size={20} />
+            <p>Live video</p>
+          </div>
+          <div className="upload-option">
+            <MdPhotoLibrary style={{ color: "#45bd62" }} size={20} />
+            <p>Photo/video</p>
+          </div>
+          <div className="upload-option">
+            <FaRegLaugh style={{ color: "#f7b928" }} size={20} />
+            <p>Feeling/activity</p>
+          </div>
         </div>
       </div>
     </>
--- src/feeds/storyReels/StoryReel.js
+++ src/feeds/storyReels/StoryReel.js
@@ -12,7 +12,11 @@ const StoryReel = () => {
             backgroundImage: `url("https://wallpapercave.com/wp/wp2775196.jpg")`,
           }}
         >
-          <Avatar />
+          <Avatar
+            src="https://tse4.mm.bing.net/th?id=OIP.wgEzlKddaEirNaX1pCDtnQHaLG&pid=Api&P=0&h=180"
+            sx={{ height: "35px", width: "35px" }}
+            style={{ border: "2px solid #2189e8" }}
+          />
           <h4>Vikash Singh</h4>
         </div>
 
@@ -22,7 +26,11 @@ const StoryReel = () => {
             backgroundImage: `url("https://wallpapercave.com/wp/wp2775306.jpg")`,
           }}
         >
-          <Avatar />
+          <Avatar
+            src="https://www.teahub.io/photos/full/108-1085401_simple-boy-pic-dp.jpg"
+            sx={{ height: "35px", width: "35px" }}
+            style={{ border: "2px solid #2189e8" }}
+          />
           <h4>Hariom Raaj</h4>
         </div>
 
@@ -32,7 +40,11 @@ const StoryReel = () => {
             backgroundImage: `url("https://i.pinimg.com/originals/66/8c/27/668c2724434c9a02dceb471ba3aafe41.jpg")`,
           }}
         >
-          <Avatar />
+          <Avatar
+            src="https://www.imagesplatform.com/mohsin_site/uploads/2019_10_09/a9a98243b190f1ba.jpg"
+            sx={{ height: "35px", width: "35px" }}
+            style={{ border: "2px solid #2189e8" }}
+          />
           <h4>Dimple singh</h4>
         </div>
 
@@ -42,7 +54,11 @@ const StoryReel = () => {
             backgroundImage: `url("http://cdn130.picsart.com/299422927035201.jpg")`,
           }}
         >
-          <Avatar />
+          <Avatar
+            src="https://wallpapercave.com/wp/wp5047332.jpg"
+            sx={{ height: "35px", width: "35px" }}
+            style={{ border: "2px solid #2189e8" }}
+          />
           <h4>Yash Pandey</h4>
         </div>
 
@@ -52,7 +68,11 @@ const StoryReel = () => {
             backgroundImage: `url("https://wallpapercave.com/wp/wp4829130.jpg")`,
           }}
         >
-          <Avatar />
+          <Avatar
+            src="https://images.statusfacebook.com/profile_pictures/boys-dp/boys-dp-142.jpg"
+            sx={{ height: "35px", width: "35px" }}
+            style={{ border: "2px solid #2189e8" }}
+          />
           <h4>Shiv Kumar</h4>
         </div>
       </div>
